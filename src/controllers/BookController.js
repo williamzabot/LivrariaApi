f7c1acb4b7ff4a91fe17d62f66411f466af72444
WILLIAM CANSI ZABOT <181810021@fspoa.br>
2023-06-06 20:40:16 -0300

    make isbn unique

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -1,12 +1,14 @@
 const registerController = require("../controllers/RegisterController");
 
 let registerCount = 0;
+let isbn = 0;
 const books = [];
 
 function registerBook(req, res) {
   const book = req.body;
-  if (book && book.isbn && book.title && book.price && book.authors) {
+  if (book && book.title && book.price && book.authors) {
     book.available = true;
+    book.isbn = isbn += 1;
     if (!book.publisher) {
       book.publisher = "";
     }
